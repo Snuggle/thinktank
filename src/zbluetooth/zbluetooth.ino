cb9b81b74458315f8e350ec3e0163969860441b5
Snuggle <snuggle@sprinkly.net>
2018-09-25 07:59:27 +0100

    Finalised bluetooth code?

--- src/zbluetooth/zbluetooth.ino
+++ src/zbluetooth/zbluetooth.ino
@@ -1,3 +1,4 @@
+#include <Servo.h>
 #define ENA 5
 #define ENB 6
 #define IN1 7
@@ -9,17 +10,19 @@
 bool state = LOW;
 char getstr;
 unsigned int speed;
+float currentAngle;
 
+Servo myservo;
 
-
-void toggleLED() {
-  state = !state;
-  digitalWrite(LED, state);
-  Serial.println("Light");
+void blinkLED() {
+  digitalWrite(LED, HIGH);
+  delay(100)
+  digitalWrite(LED, LOW);
 }
 
 void setup () {
   Serial.begin(9600);
+  myservo.attach(3);
   pinMode(LED, OUTPUT);
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
@@ -28,6 +31,8 @@ void setup () {
   pinMode(ENA, OUTPUT);
   pinMode(ENB, OUTPUT);
   speed = 100;
+  currentAngle = 80;
+  myservo.write(currentAngle);
   //move(0);
 }
 
@@ -87,9 +92,9 @@ void loop() {
     case '.': move(0, speed); break;
     case '^': move(1, speed); break;
     case 'v': move(2, speed); break;
-    case '<': move(3, speed); break;
-    case '>': move(4, speed); break;
-    case 'L': toggleLED(); break;
+    case '<': move(3, 255); break;
+    case '>': move(4, 255); break;
+    case 'L': blinkLED(); break;
     case 'U':
       speed = 255;
       break;
@@ -98,6 +103,29 @@ void loop() {
       break;
     case 'D':
       speed = 100;
+      break;
+    case 'J':
+      if (currentAngle >= 40){
+        currentAngle=currentAngle-15;
+        myservo.write(currentAngle);
+      }
+      break;
+    case 'P':
+      if (currentAngle <= 120) {
+        currentAngle=currentAngle+15;
+        myservo.write(currentAngle);
+      }
+      break;
+    case 'S':
+      for (int i=0; i<=8; i++){
+        blinkLED();
+        currentAngle=15*i;
+        myservo.write(currentAngle);
+        delay(100);
+      }
+      delay(2000);
+      currentAngle=80;
+      myservo.write(currentAngle);
       break;
     default: break;
   }
